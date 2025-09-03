7da437c2d1c6534a2bf06bcf9b81bef8cbcb51b1
Eduardo <eduardo.gomara.sagastume@gmail.com>
2025-09-03 15:20:15 -0400

    ttl for database token

--- mongo-init/01_refresh_tokens_ttl.js
+++ mongo-init/01_refresh_tokens_ttl.js
@@ -1,16 +1,15 @@
 // Runs automatically at container init (first DB creation)
 // Sets up a TTL index so tokens expire 7h after insertion
 
-const dbName = 'mydb';
-const coll = 'refreshTokens';
+const dbName = process.env.DATABASE_NAME;
+const collection = 'refreshTokens';
 
 const db = db.getSiblingDB(dbName);
 
-// Ensure collection exists
-db.createCollection(coll);
+db.createCollection(collection);
 
 // Create TTL index: expire 7 hours (25200 seconds) after createdAt
-db[coll].createIndex(
+db[collection].createIndex(
   { createdAt: 1 },
   {
     expireAfterSeconds: 25200, // 7 * 60 * 60
